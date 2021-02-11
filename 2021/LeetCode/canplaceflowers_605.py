9e232e4d90ce5d1948c1463cad2264de3dcc725e
ithieve <ithieve@yahoo.com>
2021-02-11 22:06:23 +0600

    Trying out canplaceflower from leetcode

---
+++ 2021/LeetCode/canplaceflowers_605.py
@@ -0,0 +1,11 @@
+def canplace(ls, n):
+    for i, value in enumerate(ls):
+        
+      
+
+flowerbed = [0]
+#flowerbed = [0]
+#flowerbed = [1]
+#flowerbed = [1,0,0,1,0,0,0,1]
+
+print(canplace(flowerbed, 1))
