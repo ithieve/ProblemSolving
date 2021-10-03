6b668e32a4d8da76b4a8ddcd2b121cb70cec3f40
iThieve <ithieve@yahoo.com>
2021-10-04 00:33:37 +0600

    moved everything in the workspace to old

---
+++ 2021/LeetCode/Workspace/Old/389.py
@@ -0,0 +1,16 @@
+s= "aervhe"
+t= "rhevaxe"
+
+source = {}
+test = {}
+
+if s = '' and len(y) !=0:
+    print(t)
+
+else:
+    for c in s:
+        if c not in source.keys():
+            source[c]=1
+        else:
+            source[c] = source[c] +1
+        
